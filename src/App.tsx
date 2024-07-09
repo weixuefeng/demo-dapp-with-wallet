ba2a60366e9ccd3cd6851f27a1c88a6005ec371f
pony <weixuefeng1018@gmail.com>
2024-07-09 14:04:08 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import './App.scss';
+import './trackers';
+import {THEME, TonConnectUIProvider} from "@tonconnect/ui-react";
+import {Footer} from "./components/Footer/Footer";
+import {Header} from "./components/Header/Header";
+import {TxForm} from "./components/TxForm/TxForm";
+
+function App() {
+  return (
+    // https://gateio.onelink.me/DmA6/web3?v=2&id=21d90f3666529f7d4c05efb4c9471e7f09c21b3fc0f587175841cec02466ca0d&r=%7B%22manifestUrl%22%3A%22https%3A%2F%2Fton-connect.github.io%2Fdemo-dapp-with-wallet%2Ftonconnect-manifest.json%22%2C%22items%22%3A%5B%7B%22name%22%3A%22ton_addr%22%7D%5D%7D&ret=none
+    <TonConnectUIProvider
+      manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
+      uiPreferences={{theme: THEME.DARK}}
+      walletsListConfiguration={{
+        includeWallets: [
+          {
+            appName: "GateWallet",
+            name: "GateWallet",
+            imageUrl: "https://www.gate.io/images/login/qrcode_center_icon.svg",
+            aboutUrl: "https://gate.io",
+            universalLink: "https://gateio.onelink.me/DmA6/web3",
+            jsBridgeKey: "gatetonwallet",
+            deepLink: "https://gateio.onelink.me/DmA6/web3",
+            bridgeUrl: "https://web3-dapp-test.gatebigp.com/tonbridge_api/bridge/v1",
+            platforms: ["ios", "android"]
+          },
+        ]
+      }}
+      actionsConfiguration={{
+        twaReturnUrl: 'https://t.me/tc_twa_demo_bot/start'
+      }}
+    >
+      <div className="app">
+        <Header/>
+        <TxForm/>
+        {/*<TonProofDemo />*/}
+        <Footer/>
+      </div>
+    </TonConnectUIProvider>
+  )
+}
+
+export default App
