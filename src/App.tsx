e02b41ae5f0e0a96164eb6cd68adfc728fd4f7f9
S_PONY <s_pony@gate.me>
2024-10-23 17:56:51 +0800

    fix: app name

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
       walletsListConfiguration={{
         includeWallets: [
           {
-            appName: "GateWallet2",
-            name: "GateWallet2",
+            appName: "GateWallet",
+            name: "GateWallet",
             imageUrl: "https://www.gate.io/images/login/qrcode_center_icon.svg",
             aboutUrl: "https://gate.io",
             universalLink: "https://gateio.onelink.me/DmA6/web3",
